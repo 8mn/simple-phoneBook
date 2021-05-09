f784e0aed0a6d67317865a18e3ae5e7eb6fc4ea9
calmighted <calmighted@gmail.com>
2021-05-09 20:11:13 +0530

    fixed addition to phone number list

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: 9827163498
-  }
+    { name: 'Arto Hellas', number: '9827163498'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
-  const personList = persons.map((person) => <li key={person.name}>{person.name}: {person.number}{console.log(person.name)}</li>)
+  const personList = persons.map((person) => <li key={person.name}>{person.name}: {person.number}</li>)
+
+  const handleName = (e) => {
+    setNewName(e.target.value)
+  }
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  const AddNumToList = (Name,Number) => {
+    const newPerson = {
+      name:Name,
+      number:Number
+    }
+    // const newPerson = {Name,Number}
+    console.log(newPerson)
+    setPersons(persons.concat(newPerson))
+    console.log(persons)
+  }
+
+
 
   return (
     <div className={classes.container}>
@@ -18,13 +37,15 @@ const App = () => {
       <section className={classes.phoneList}>
       <form>
         <div className={classes.NameInputContainer}>
-          <span className={classes.Name}> Name:</span> <input className={classes.NameInput} />
+          <span className={classes.Name}> Name:</span> 
+          <input className={classes.NameInput}  onChange={handleName}/>
         </div>
         <div className={classes.PhoneInputContainer}>
-          <span className={classes.Number}> Number:</span> <input className={classes.NameInput} />
+          <span className={classes.Number}> Number:</span> 
+          <input className={classes.NameInput} onChange={handleNumber} />
         </div>
         <div>
-          <button type="submit">Add</button>
+          <button type="button" onClick={() => AddNumToList(newName,newNumber)}>Add</button>
         </div>
       </form>
       <div className={classes.phoneNumberContainer}>
