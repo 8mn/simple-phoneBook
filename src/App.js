89172aaa465c4660ece942e7fe994c32cd62c95a
calmighted <calmighted@gmail.com>
2021-05-10 11:37:23 +0530

    added search function with state

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import {BiSearch,BiTrash} from "react-icons/bi";
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '9827163498'},
+    { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
-  const personList = persons.map((person) => <li key={person.name}>{person.name}: {person.number}<button className={classes.delete}><BiTrash /></button></li>)
+  const[search,setsearch] = useState('')
+
+  const searchFilter = persons.filter(person => person.name.toLowerCase().startsWith(search))
+
+  const personList = searchFilter.map((person) => <li key={person.name}>{person.name}:{person.number}<button className={classes.delete}><BiTrash /></button></li>)
 
   const handleName = (e) => {
     setNewName(e.target.value)
@@ -85,7 +91,7 @@ const App = () => {
       </form>
       <div className={classes.phoneNumberContainer}>
       <div className={classes.searchBar}>
-        <input type="text" className={classes.search} />
+        <input type="text" className={classes.search} onChange={e => setsearch(e.target.value)}/>
         <BiSearch className={classes.searchIcon} />
       </div>
       
